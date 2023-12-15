783dc36a706d922cf66724c2adff8530211bf76c
Rob Marscher <rob@robmarscher.com>
2023-12-14 23:10:09 -0500

    Export ClientObjectProps

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export { DataApi, FileMakerError, isOttoAuth };
 export * from "./utils/utils.js";
 
 export type { TokenStoreDefinitions } from "./tokenStore/types.js"
+export type { ClientObjectProps } from "./client.js"
 export default DataApi;
