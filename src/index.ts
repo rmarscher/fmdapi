8ab02fa55f4cff705d2c835895bc8d0a6f614a35
Rob Marscher <rob@robmarscher.com>
2023-12-14 23:01:01 -0500

    Export TokenStoreDefinitions

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ import { DataApi, FileMakerError, isOttoAuth } from "./client.js";
 export { DataApi, FileMakerError, isOttoAuth };
 export * from "./utils/utils.js";
 
+export type { TokenStoreDefinitions } from "./tokenStore/types.js"
 export default DataApi;
