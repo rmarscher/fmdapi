ad94f44f925ca798d030ddacdb9cf363ba936261
Rob Marscher <rob@robmarscher.com>
2023-12-14 23:20:13 -0500

    3.2.14-slack-next-gen.8

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import { DataApi, FileMakerError } from "./client.js";
 export type { TokenStoreDefinitions } from "./tokenStore/types.js";
+export type { ClientObjectProps } from "./client.js";
 export { FileMakerError, DataApi };
 export { removeFMTableNames } from "./utils/utils.js";
 export default DataApi;
