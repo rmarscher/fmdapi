5d7e5e337675921d39d0f4b7bf2fe0bb02d8ef81
Rob Marscher <rob@robmarscher.com>
2023-12-14 23:01:55 -0500

    Build 3.2.14-slack-next-gen.6

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 import { DataApi, FileMakerError } from "./client.js";
+export type { TokenStoreDefinitions } from "./tokenStore/types.js";
 export { FileMakerError, DataApi };
 export { removeFMTableNames } from "./utils/utils.js";
 export default DataApi;
