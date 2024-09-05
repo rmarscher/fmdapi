b36b596559c7f875156c5366a95f1758f3c67a9b
Rob Marscher <rob@robmarscher.com>
2024-09-04 23:06:17 -0400

    Export more types

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,7 @@ export * from "./utils/utils.js";
 export { FetchAdapter } from "./adapters/fetch.js";
 export { OttoAdapter, type OttoAPIKey } from "./adapters/otto.js";
 
+export type { TokenStoreDefinitions } from "./tokenStore/types.js"
+export type * from "./client-types.js"
+export type { ClientObjectProps } from "./client.js"
 export default DataApi;
